b3c5aa225018b53c30545839ec13fa86bb559e45
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-20 18:58:40 +0800

    finished the print full name in upper case program

--- print_full_name_in_upper_case.py
+++ print_full_name_in_upper_case.py
@@ -1,4 +1,6 @@
 # input user's full name
-full_name= input("Please input your full name")
+full_name= input("Please input your full name: ")
 # make the full name upper case
+upper_cased_full_name= full_name.upper()
 # print full name
+print (upper_cased_full_name)
