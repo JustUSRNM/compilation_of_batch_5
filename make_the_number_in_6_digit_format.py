fc19f81fb365ec80c414fc0e14c3c02a99287aa9
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-20 18:54:35 +0800

    finished the convert to 6 digit format program

--- make_the_number_in_6_digit_format.py
+++ make_the_number_in_6_digit_format.py
@@ -1,4 +1,9 @@
 # input a 1-1000 number
-inputted_number= int(input("Please input a number from 1-1000"))
+inputted_number= int(input("Please input a number from 1-1000: "))
 # loop: while the number's length is less than 6 add zero at the beggining
-# print the inputed number in 6 digit format
+if 1<= inputted_number <= 1000:
+    six_digit_number= str(inputted_number)
+    while len(six_digit_number) != 6:
+        six_digit_number= "0" + six_digit_number
+    # print the inputed number in 6 digit format
+    print (six_digit_number)
