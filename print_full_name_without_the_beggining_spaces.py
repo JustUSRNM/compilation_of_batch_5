605e59e440de036fe79d22cca5753bfc96862b29
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-20 18:43:14 +0800

    finished the print without the beggining spaces program

--- print_full_name_without_the_beggining_spaces.py
+++ print_full_name_without_the_beggining_spaces.py
@@ -1,4 +1,6 @@
 # input user's full name
-full_name= input("Please input your full name with spaces at the very beggining")
+full_name= input("Please input your full name with spaces at the very beggining: ")
 # remove the beggining spaces
+cleaned_full_name= full_name.lstrip()
 # print full name
+print (cleaned_full_name)
