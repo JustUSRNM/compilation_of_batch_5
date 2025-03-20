521f27d07571a45d57ac6321ea67a1f6b2155242
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-20 19:05:55 +0800

    finished the print full name in propper casing program; finished first five programs

--- print_full_name_in_proper_casing.py
+++ print_full_name_in_proper_casing.py
@@ -1,4 +1,6 @@
 # input user's full name
-full_name= input("Please input your full name in improper casing")
+full_name= input("Please input your full name in improper casing: ")
 # make the full name in proper casing
+cleaned_full_name= full_name.title()
 # print full name
+print (cleaned_full_name)
