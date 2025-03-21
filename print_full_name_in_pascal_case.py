683319e5e1fcfd5599ad6362b3dac0eb31d5dd64
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-21 15:31:44 +0800

    finished the print full name in pascal case program

--- print_full_name_in_pascal_case.py
+++ print_full_name_in_pascal_case.py
@@ -1,5 +1,8 @@
 # input user's full name
-full_name= input("Please input your full name: ")
+full_name= input("Please input your full name in incorrect casing: ")
 # make the full name into proper casing (title case)
+titled_full_name= full_name.title()
 # remove the spaces
+pascal_case= titled_full_name.replace(' ','')
 # print full name in pascal casing
+print (pascal_case)
