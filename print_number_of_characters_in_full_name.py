331c1f081a603e6f64343c804debb187d39dda96
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-21 15:25:26 +0800

    replaced the replace command to strip; finished the print the number of characters program

--- print_number_of_characters_in_full_name.py
+++ print_number_of_characters_in_full_name.py
@@ -1,7 +1,8 @@
 # input user's full name
 full_name= input("Please input your full name: ")
 # remove the spaces in the full name
-no_spaces_full_name= full_name.replace(" ","")
-print (no_spaces_full_name)
+no_spaces_full_name= full_name.strip()
 # count the number characters in the full name
+number_of_characters= len(no_spaces_full_name)
 # print the number of characters of the full name
+print (number_of_characters)
