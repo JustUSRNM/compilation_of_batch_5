2665453c360aa04bcacb2b221113761dc66ddc11
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-21 15:22:30 +0800

    added a command to remove the spaces between words

--- print_number_of_characters_in_full_name.py
+++ print_number_of_characters_in_full_name.py
@@ -1,5 +1,7 @@
 # input user's full name
 full_name= input("Please input your full name: ")
 # remove the spaces in the full name
+no_spaces_full_name= full_name.replace(" ","")
+print (no_spaces_full_name)
 # count the number characters in the full name
 # print the number of characters of the full name
