a8031ec854ea4214de689ef3b671e7d4899eab75
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-21 15:19:14 +0800

    finished the print number of words program

--- print_the_number_of_words_in_the_statement.py
+++ print_the_number_of_words_in_the_statement.py
@@ -1,4 +1,7 @@
 # input user's statement
 user_statement = input("Please input a statement: ")
 # split the statement to single words then count the words
+splitted_statement= user_statement.split(' ')
+number_of_words= len(splitted_statement)
 # print the number of words
+print (number_of_words)
