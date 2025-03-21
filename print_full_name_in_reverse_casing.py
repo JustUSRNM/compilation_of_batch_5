5e77e001911801fd15c64efb115b0b4a180e4e48
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-21 15:13:54 +0800

    finished the print in reverse casing program

--- print_full_name_in_reverse_casing.py
+++ print_full_name_in_reverse_casing.py
@@ -1,4 +1,6 @@
 # input user's full name
 full_name= input("Please input your full name: ")
 # apply reverse casing to the full name
+reversed_full_name= full_name.swapcase()
 # print full name reverse casing
+print (reversed_full_name)
