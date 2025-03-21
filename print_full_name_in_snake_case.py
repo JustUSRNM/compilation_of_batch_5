74849402b0de07a6883782d9a967b3198f02892d
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-21 15:36:42 +0800

    added the command to remove whitespace and to turn the full name to lower case

--- print_full_name_in_snake_case.py
+++ print_full_name_in_snake_case.py
@@ -1,6 +1,9 @@
 # input user's full name
-full_name= input("Please input your full name: ")
+full_name= input("Please input your full name in incorrect casing: ")
 # remove the leading and trailing spaces
+no_spaces_full_name= full_name.strip()
 # make the full name in lower case
+lower_case_full_name= full_name.lower()
+print (lower_case_full_name)
 # replace the spaces with _
 # print full name reverse casing
